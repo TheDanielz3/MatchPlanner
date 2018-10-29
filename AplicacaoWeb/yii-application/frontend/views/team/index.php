43ba33492d0ed9909d7eef8d154e0ab4ce213f03
dalpendre <2170324@my.ipleiria.pt>
2018-10-29 16:16:25 -0100

    Framework atualizado com modelos e crud teams

---
+++ AplicacaoWeb/yii-application/frontend/views/team/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\TeamSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Teams';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="team-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Team', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'team_name',
+            'creation_date',
+            'members',
+            'email:email',
+            //'username',
+            //'password',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
