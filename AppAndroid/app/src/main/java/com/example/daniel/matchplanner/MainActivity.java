c7ab36924f8b4a23362a8e06ea8351c8f5d2c34b
Daniel Batista <2171836@my.ipleiria.pt>
2018-10-30 10:03:34 +0000

    Adding AndroidApp
    Co-Authored-By: dalpendre <dalpendre@users.noreply.github.com>

---
+++ AppAndroid/app/src/main/java/com/example/daniel/matchplanner/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.daniel.matchplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//Todo: Onde se cria as atividades dos diversos objetos
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
