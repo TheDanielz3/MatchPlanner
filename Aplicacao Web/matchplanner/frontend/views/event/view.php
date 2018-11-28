6ffd797a1e3405674723961b8c1c6b0659d4b1b9
dalpendre <2170324@my.ipleiria.pt>
2018-11-28 16:30:48 -0100

    Framework atualizada
    Framework atualizada com novo nome e ver perfil e validação de signup / login caso user dê logout antes de fazer solo / team register

---
+++ Aplicacao Web/matchplanner/frontend/views/event/view.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Event */
+
+//Link para voltar à main view
+$mainView = Url::toRoute('site/operations', true);
+echo "<br/>";
+echo "" . Html::a('Go back to main view', $mainView);
+
+$this->title = $model->event_name;
+$this->params['breadcrumbs'][] = ['label' => 'Events', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="event-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+           // 'id',
+            'event_name',
+            'begin_date',
+            'end_date',
+            'description',
+            //'user_id',
+            //'team_id',
+        ],
+    ]) ?>
+
+</div>
